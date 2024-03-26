386a88f508e93de7c2b14fb9114d890cc07210d9
nogueiiiiiiiiira <158315598+nogueiiiiiiiiira@users.noreply.github.com>
2024-03-26 20:46:09 -0300

    Update Card.jsx

---
+++ Atividade/src/components/Card.jsx
@@ -0,0 +1,16 @@
+import style from './Card.module.css'
+
+export default function Card(props){
+    return(
+      
+      <div className={style.wrapCard}>
+        <div className={style.Card}>
+          <h1 className={style.cardTitle}>{props.title}</h1>
+          <img className={style.cardImgTop} src={props.imgSrc} alt={props.title} width={300} height={200}/>
+          <div className={style.cardBody}>
+              <p className={cardText}>props.desc/p>
+
+            </div>
+          </div>
+    )
+}
