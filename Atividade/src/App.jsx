c7fd699672cd51f790be55cf3ee6238562dfe676
nogueiiiiiiiiira <158315598+nogueiiiiiiiiira@users.noreply.github.com>
2024-03-26 22:25:12 -0300

    Update App.jsx

--- Atividade/src/App.jsx
+++ Atividade/src/App.jsx
@@ -4,7 +4,7 @@ import Menu from './components/Menu'
 export default function App() {
 
   return (
-    <div className={style.wrapHome]}>
+    <div className={style.wrapHome}>
         <Menu />
       <h1 className={style.firstPageHeading}>Home!</h1>
     </div>
