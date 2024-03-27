6bf77e38c94feee5736cb76f8c3a43d8868b2012
nogueiiiiiiiiira <karencristininogueira@gmail.com>
2024-03-27 16:36:17 -0300

    Atualizações

--- Atividade/src/App.jsx
+++ Atividade/src/App.jsx
@@ -1,6 +1,9 @@
 import style from './App.module.css'
 import Menu from './components/Menu'
 
+import prof from './assets/prof.jpg'
+import Card from './components/Card'
+
 export default function App() {
 
   return (
@@ -8,7 +11,19 @@ export default function App() {
         <Menu />
         <div className={style.firstPageHeading}>
           <h1>HOME!</h1>
-          <p className={style.pHome} >Seja bem vindo(a) à atividade que o professor André pediu!</p>
+          <p className={style.pHome} >Seja bem vindo(a) à atividade que o professor André pediu só para poder descansar!</p>
+        <br />
+        <br />
+        <div>
+          <Card 
+          title='PROFESSOR ANDRE' 
+          imgSrc={prof} 
+          desc='' 
+          value = "Aumente o salário dele!"
+          />
+          </div>
+
+
       </div>
     </div>
   )
